1baffe924d9e14e4d0a5d93779728dcc3b4a4c0c
Deborah <soccio.deborah@gmail.com>
2024-01-12 10:30:24 +0100

    modifs app

--- app.py
+++ app.py
@@ -19,6 +19,21 @@ page = st.sidebar.radio("Navigation", ["Home", "Informations","Estimation"])
 
 if page == "Home":
     st.write("<h1>Bienvenue sur Assur'Aimant</h1>", unsafe_allow_html=True)
+        # Création d'une mise en page avec 3 colonnes
+    col1, col2, col3 = st.columns(3)
+
+    # Ajout d'émojis et de texte dans chaque colonne
+    with col1:
+        st.markdown('😃 Conseil Data & IA Solutions')
+        st.write("Nous sommes une entreprise dynamique spécialisée en data science et intelligence artificielle. Notre mission est d'apporter des solutions innovantes et efficaces pour transformer les industries, avec un focus particulier sur le secteur des assurances.")
+
+    with col2:
+        st.markdown('📚')
+        st.write('Rubrique 2')
+
+    with col3:
+        st.markdown('🌍')
+        st.write('Rubrique 3')
     st.write("<div class='test'><h2>Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux</h2></div>", unsafe_allow_html=True)
     st.image('courtier-assurance.jpg', width=500)
 
@@ -42,11 +57,11 @@ elif page == "Estimation":
 
     # form
     with st.form(key='mon_formulaire'):
-        age = st.number_input(label="Entrez votre age", min_value=0, max_value=100)
-        children = st.number_input(label="Nombre d'enfants", min_value=0, max_value=100)
+        age = st.slider('Indiquez votre âge', 0, 100, 0)
+        children = st.slider("Combien d'enfants à charge ?", 0, 20, 0)
         sex = st.selectbox(label='Genre', options=['female', 'male'])
         poids = st.number_input(label="Indiquez votre poids", min_value=0.0, max_value=100.0, format="%.2f")
-        taille = st.number_input(label="Indiquez votre taille", min_value=0.0, max_value=100.0, format="%.2f")
+        taille = st.slider('Indiquez votre taille', 0.0, 3.0, 0.0)
         smoker = st.selectbox(label='Etes-vous fumeur ?', options=['yes', 'no'])
         region = st.selectbox(label='Région', options=['southeast', 'southwest','northeast', 'northwest'])
     
@@ -70,7 +85,7 @@ st.markdown('''
     # background-repeat: no-repeat !important;
     # background-size: cover !important;
     # height: 100vh;
-    background-color: rgb(205, 197, 246);  
+    background-color: rgb(228, 228, 252) ;  
 }
 
 //sidebar
@@ -105,6 +120,10 @@ h2 {
             
 .test{
     padding-bottom : 50px;
+}
+            
+.StyledThumbValue{
+    color: black;
 }
 </style>
 ''', unsafe_allow_html=True)
